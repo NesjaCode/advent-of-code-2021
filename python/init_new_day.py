fee50bc22ebddd64b5d2b66d98ff0aac66aa35c9
NesjaCode <58387039+NesjaCode@users.noreply.github.com>
2021-12-01 12:57:46 +0100

    Change init script and template file

--- python/init_new_day.py
+++ python/init_new_day.py
@@ -1,6 +1,5 @@
 import os
 import requests
-import shutil
 
 # Create directory for the new day
 input_day = input("No. day --> ")
@@ -18,13 +17,22 @@ print("Pulling input from: " + http_path + "...")
 cookies = {"session": aoc_session}
 response = requests.get(http_path, cookies=cookies)
 
-file = open(day_path + "/input.txt", "w")
+file = open("../input" + day_path[1:] + ".txt", "w")
 file.write("\n".join(filter(lambda x: x != "", response.text.split("\n"))))
 file.close()
 
-# Copy the template file into the new directory. Once for each part.
-print("Copying template python structure...")
-shutil.copyfile("part_template.py", day_path + "/part_1.py")
-shutil.copyfile("part_template.py", day_path + "/part_2.py")
+print("Copying template files...")
+# Copy template files into new day directory
+with open("part_template.py", "r") as file:
+    template = "".join(file.readlines()).split("{}")
+
+template[0] += input_day
+file = open(day_path + "/part_1.py", "w")
+file.write("".join(template))
+file.close()
+
+file = open(day_path + "/part_2.py", "w")
+file.write("".join(template))
+file.close()
 
 print("Done!")
--- python/part_template.py
+++ python/part_template.py
@@ -1,3 +1,3 @@
 def parse_input():
-    with open("input.txt", "r") as file:
+    with open("../../input/day_{}.txt", "r") as file:
         return list(map(lambda x: x.strip(), file.readlines()))
